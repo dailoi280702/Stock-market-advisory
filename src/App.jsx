154ff0fa31c8bafc0e0d71b102772c276efd0c77
Lloyd <dailoi280702@gmail.com>
2023-12-09 19:50:00 +0700

    feat: add signin signup route

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,15 @@ import {
 } from 'react-router-dom';
 import './App.css';
 import Layout from './components/Layout';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<Layout />}>
       <Route path="*" element={<div>not found!</div>} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
     </Route>
   )
 );
