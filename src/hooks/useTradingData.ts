e79be78bad6d7b6a5641bd3e489ef4a4c57c34e3
Lloyd <dailoi280702@gmail.com>
2023-12-22 00:07:35 +0700

    fix: convert trading data to float

--- src/hooks/useTradingData.ts
+++ src/hooks/useTradingData.ts
@@ -1,21 +1,35 @@
 import { useEffect, useState } from 'react';
-import { ServerState } from './useWishlist';
 
-export type AdjustedData = {
+type AdjustedData = {
   [date: string]: {
-    '1. open': string;
-    '2. high': string;
-    '3. low': string;
-    '4. close': string;
-    '5. adjusted close': string;
-    '6. volume': string;
-    '7. dividend amount': string;
+    '1. open': number;
+    '2. high': number;
+    '3. low': number;
+    '4. close': number;
+    '5. adjusted close': number;
+    '6. volume': number;
+    '7. dividend amount': number;
   };
 };
 
+type ServerState<T> =
+  | {
+      state: 'loading';
+    }
+  | {
+      state: 'success';
+      data: T;
+    }
+  | {
+      state: 'error';
+      error: Error;
+    };
+
 export const useTradingData = (symbol: string) => {
   const [period, setPeriod] = useState<'daily' | 'weekly' | 'monthly'>('daily');
-  const [tradingData, setTradingData] = useState<ServerState<AdjustedData>>({ state: 'loading' });
+  const [tradingData, setTradingData] = useState<ServerState<AdjustedData>>({
+    state: 'loading'
+  });
 
   useEffect(() => {
     const fetchTradingData = async () => {
@@ -37,20 +51,40 @@ export const useTradingData = (symbol: string) => {
         const response = await fetch(apiUrl);
         const data = await response.json();
 
+        // Extract data based on the selected period
         const timeSeriesKey = `${
           apiFunction === 'TIME_SERIES_DAILY_ADJUSTED'
             ? 'Time Series (Daily)'
             : apiFunction === 'TIME_SERIES_WEEKLY_ADJUSTED'
-            ? 'Weekly Adjusted Time Series'
+            ? 'Weekly Time Series'
             : 'Monthly Adjusted Time Series'
         }`;
 
-        const extractedData: AdjustedData = data[timeSeriesKey];
+        const rawData = data[timeSeriesKey];
+
+        // Convert string values to floats
+        const extractedData: AdjustedData = Object.entries(rawData).reduce(
+          (result, [date, values]) => {
+            result[date] = {
+              '1. open': parseFloat((values as Record<string, string>)['1. open']),
+              '2. high': parseFloat((values as Record<string, string>)['2. high']),
+              '3. low': parseFloat((values as Record<string, string>)['3. low']),
+              '4. close': parseFloat((values as Record<string, string>)['4. close']),
+              '5. adjusted close': parseFloat(
+                (values as Record<string, string>)['5. adjusted close']
+              ),
+              '6. volume': parseFloat((values as Record<string, string>)['6. volume']),
+              '7. dividend amount': parseFloat(
+                (values as Record<string, string>)['7. dividend amount']
+              )
+            };
+            return result;
+          },
+          {} as AdjustedData
+        );
 
         setTradingData({ state: 'success', data: extractedData });
       } catch (e) {
-        console.log(e);
-
         setTradingData({ state: 'error', error: e as Error });
       }
     };
