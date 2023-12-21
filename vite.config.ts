2fe44dd842aa9dbd19262e92838a68fcca5ab1a5
Lloyd <dailoi280702@gmail.com>
2023-12-21 15:38:15 +0700

    fix: vite not load ts config paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   optimizeDeps: {
     esbuildOptions: {
       // Node.js global to browser globalThis
